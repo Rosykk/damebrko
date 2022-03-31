5fe8c5e03af9369330866ad23b8be7ef70ffa970
Rosykk <48493455+Rosykk@users.noreply.github.com>
2022-03-31 19:47:42 +0200

    1.21
    Finished sideBar

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,13 +1,22 @@
 let x = document.getElementById("sideBar");
 let l = document.getElementById("sideBarLinks");
 
+function openNav() {
+    document.getElementById("sideBar").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("sideBar").style.width = "0";
+}
+
 function myFunction() {
-    if (x.className === "sidebar.inactive") {
-        x.className = "sidebar";
+    if (x.className === "sidebar hidden") {
+        x.className = "sidebar visible";
         l.className = "visible";
     } 
     else {
-        x.className = "sidebar.inactive";
+        x.className = "sidebar hidden";
         l.className = "hidden";
+        
     }
 }
