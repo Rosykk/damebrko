59934e74f3adaa6e8a0278f22d0ae7f3d3d9eeef
Rosykk <48493455+Rosykk@users.noreply.github.com>
2022-03-31 12:58:49 +0200

    1.13
    Added some JavaScript, added redirect to the final page after "ordering", changed YouTube "palim jedno brko" to .mp3 since it's private on YouTube, minor design changes.

--- js/autofill.js
+++ js/autofill.js
@@ -1,11 +1,30 @@
-    function initAutocomplete() {
-      const options = {
-        componentRestrictions: { country: 'cs' },
-        fields: ['address'],
-        strictBounds: true
-      };
+window.onload = function WindowLoad(event) {
+  disableButton();
+}
 
-      const input = document.getElementById('search');
-      const searchBox = new google.maps.places.Autocomplete(input);
-      searchBox.setComponentRestrictions({'country': ['cz', 'sk']});
-    }
+function trigger() {
+    let input = document.getElementById('search');
+    let regex = /^\S+$/;
+
+    ( ( regex.test(input.value) == true ) ? enableButton() : disableButton() );
+}
+
+function initAutocomplete() {
+  const options = {
+    componentRestrictions: { country: 'cs' },
+    fields: ['address'],
+    strictBounds: true
+  };
+
+  const input = document.getElementById('search');
+  const searchBox = new google.maps.places.Autocomplete(input);
+  searchBox.setComponentRestrictions({'country': ['cz', 'sk']});
+}
+
+function disableButton() {
+  document.getElementById("button").disabled = true;
+}
+
+function enableButton() {
+  document.getElementById("button").disabled = false;
+}
