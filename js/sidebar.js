1cd9d8d1a99ce3a3ef20e90f6c7f0535e7a42c29
Rosykk <48493455+Rosykk@users.noreply.github.com>
2022-03-31 17:26:43 +0200

    1.17
    Test of sideBar and navBar animations.

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,1 +1,14 @@
-function myFunction() { var x = document.getElementById("respNavbar"); if (x.className === "topnav") {x.className += " responsive";} else {x.className = "topnav";}}
+function myFunction() {
+    var x = document.getElementById("sideBar");
+    var l = document.getElementById("sideBarLinks");
+
+    if (x.className === "sidebar.inactive") {
+        x.style.width = 200;
+        x.className = "sidebar";
+        l.className = "visible";
+    } 
+    else {
+        x.className = "sidebar.inactive";
+        l.className = "hidden";
+    }
+}
