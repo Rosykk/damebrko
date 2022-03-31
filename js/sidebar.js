dd2b97104c7ba70f44393357132ec86513e195a5
Rosykk <48493455+Rosykk@users.noreply.github.com>
2022-03-31 19:53:38 +0200

    1.22
    hotfix

--- js/sidebar.js
+++ js/sidebar.js
@@ -9,14 +9,14 @@ function closeNav() {
     document.getElementById("sideBar").style.width = "0";
 }
 
-function myFunction() {
-    if (x.className === "sidebar hidden") {
-        x.className = "sidebar visible";
-        l.className = "visible";
-    } 
-    else {
-        x.className = "sidebar hidden";
-        l.className = "hidden";
+// function myFunction() {
+//     if (x.className === "sidebar hidden") {
+//         x.className = "sidebar visible";
+//         l.className = "visible";
+//     } 
+//     else {
+//         x.className = "sidebar hidden";
+//         l.className = "hidden";
         
-    }
-}
+//     }
+// }
