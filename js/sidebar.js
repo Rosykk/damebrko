59224d75864e5b36e3df53b26ef1b88e128c426c
Rosykk <48493455+Rosykk@users.noreply.github.com>
2022-04-01 19:18:30 +0200

    1.26
    changed nav/sidebar settings.

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,9 +1,23 @@
-let x = document.getElementById("sideBar");
+let x = document.getElementById('sideBar');
+let b = document.getElementById('bars');
+
+let r = document.getElementById('bars');
+
+function opCloseNav() {
+    if ( r.className.includes('reopen') ) { 
+        openNav();
+        r.className = 'icon fa-lg weedify'; 
+    }
+    else {
+        r.className += ' reopen';
+        closeNav();
+    }
+}
 
 function openNav() {
-    document.getElementById("sideBar").style.width = "250px";
+    document.getElementById('sideBar').style.width = '250px';
 }
 
 function closeNav() {
-    document.getElementById("sideBar").style.width = "0";
+    document.getElementById('sideBar').style.width = '0';
 }
