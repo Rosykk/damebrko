35ca8e1f85d679bab4944322d9b0c9f9a1c53a0d
Rosykk <48493455+Rosykk@users.noreply.github.com>
2022-04-02 05:35:14 +0200

    1.45
    yet another fbase repair

--- public/js/fbase.js
+++ public/js/fbase.js
@@ -1,15 +1,13 @@
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js';
 
 const firebaseConfig = {
   apiKey: "AIzaSyDHfW5YjupazC_IUXRDAr1Jin8CuBLHv_A",
   authDomain: "sunny-wavelet-272116.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
   projectId: "sunny-wavelet-272116",
   storageBucket: "sunny-wavelet-272116.appspot.com",
   messagingSenderId: "1013759273521",
-  appId: "11013759273521web625e8c3fd356561e140310",
+  appId: "1:1013759273521:web:625e8c3fd356561e140310",
   measurementId: "G-BCJF9P4Y25"
 };
 
