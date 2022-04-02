a3f9364d8b3c810a39a3a8231536206d6ba14ba0
Rosykk <48493455+Rosykk@users.noreply.github.com>
2022-04-02 05:21:19 +0200

    Update fbase.js

--- public/js/fbase.js
+++ public/js/fbase.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js';
 import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.6.10/firebase-analytics.js';
 
